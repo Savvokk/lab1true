54da13c3c78f15b07d04b841f466cd9ff0313ca7
Yariz <oneclass70@gmail.com>
2023-03-03 00:06:19 +0300

    Add file

---
+++ 5.py
@@ -0,0 +1,6 @@
+n = int(input('введите колличество слов'))
+text = ''
+for i in range(n):
+    a = input('Введите слово')
+    text +=str(a) + " "
+print(text)
---
+++ main.py
@@ -0,0 +1,6 @@
+pas1 = input()
+pas2 = input()
+if pas1 == pas2:
+    print("пароль принят")
+else:
+    print("Пароль не принят")
